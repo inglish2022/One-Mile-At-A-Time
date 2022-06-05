e22cf969f373ee81e3a70dba52bec0a8a143fbe9
Inglish Foust <99756614+inglish2022@users.noreply.github.com>
2022-06-05 11:01:54 -0400

    fix express-static placement

--- server.js
+++ server.js
@@ -23,13 +23,13 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 //turn on routes
 app.use(routes);
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars')
-app.use(express.static(path.join(__dirname, 'public')));
+
 //turn on connection to db and server
 
 app.listen(PORT, () => {
