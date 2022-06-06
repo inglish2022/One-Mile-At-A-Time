a5e238560428cce24c7626328adc31d901db4fb2
Inglish Foust <99756614+inglish2022@users.noreply.github.com>
2022-06-06 07:44:18 -0400

    typos found

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,8 +4,8 @@ const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
 
-router.use('./users', userRoutes);
-router.use('./posts', postRoutes);
-router.use('./comments', commentRoutes);
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
