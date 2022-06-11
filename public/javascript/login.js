293d2b2541ba6b3659d6166f4990d8f6b9ba8aeb
Inglish Foust <99756614+inglish2022@users.noreply.github.com>
2022-06-11 19:40:28 -0400

    fix login

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,10 +1,8 @@
 async function loginFormHandler(event) {
     event.preventDefault();
-
+    console.log('working')
     const username = document.querySelector('#username-login').value.trim();
-    
     const password = document.querySelector('#password-login').value.trim();
-
     if (username && password) {
         const response = await fetch('/api/user/login', {
             method: 'POST',
@@ -13,17 +11,18 @@ async function loginFormHandler(event) {
                 password
             }),
             headers: { 'Content-Type': 'application/json' }
-        });
+        })
     //     .then(function() {
-    //         document.location.replace('/dashboard');
+    //         document.location.replace(‘/dashboard’);
     // })
     // .catch(err => console.log(err));
-    if (response.ok) {
-        document.location.replace('./dashboard');
+    if (response.ok)  {
+        document.location.replace('/dashboard');
     } else {
         alert(response.statusText)
     }
+    // .catch(err => console.log(err));
+    }
+ };
 
-};
-
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler)};
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
