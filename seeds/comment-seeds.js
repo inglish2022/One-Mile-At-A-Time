6dad29b9b9dff68fcb9593e9fea1c1af1e33972d
Inglish Foust <99756614+inglish2022@users.noreply.github.com>
2022-06-11 09:27:44 -0400

    fix comments

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -19,7 +19,27 @@ const commentData = [{
         comment_text: "I would love to go back here someday with my family",
         user_id: 4,
         post_id: 4
-    }
+    },
+    {
+        comment_text: "Is there a lot to do besides skiing in Colorado?",
+        user_id: 1,
+        post_id: 3
+    },
+    {
+        comment_text: "Is there much to do besides ride roller coasters there?",
+        user_id: 2,
+        post_id: 4
+    },
+    {
+        comment_text: "I heard that there is a lot of snorkeling and scubadiving that happens there too!!",
+        user_id: 3,
+        post_id: 1
+    },
+    {
+        comment_text: "This seems like a cheap and fun time for kids all around!!",
+        user_id: 4,
+        post_id: 2
+    },
 ];
 
 const seedComments = () => Comment.bulkCreate(commentData);
